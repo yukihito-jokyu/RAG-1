1cd3a84e639a95e80c26e3f4db97d5a186d448d2
yukihito <ok220020@oks.iput.ac.jp>
2024-09-18 16:27:43 +0900

    検索の検証を行うコードを作成

--- src/rag_1/main.py
+++ src/rag_1/main.py
@@ -1,23 +1,23 @@
 from rag_1.generation import GoogleGemini
 from rag_1.search import NormalSearch
 
-# search = NormalSearch()
+search = NormalSearch()
 # search.save()
 
-search = NormalSearch.load()
+# search = NormalSearch.load()
 
-gemini = GoogleGemini()
+# gemini = GoogleGemini()
 
 # query = "小説「のんきな患者」で、吉田が病院の食堂で出会った付添婦が勧めた薬の材料は何ですか？"
-query = "小説「のんきな患者」で、主人公の吉田の患部は主にどこですか？"
+# query = "小説「のんきな患者」で、主人公の吉田の患部は主にどこですか？"
 
-tops = 2
+# tops = 2
 
-documents = search.search(query=query, tops=tops)
+# documents = search.search(query=query, tops=tops)
 
-print(documents)
+# print(documents)
 
-results = gemini.generation(query=query, documents=documents)
+# results = gemini.generation(query=query, documents=documents)
 
-print(results)
-print(results.content)
+# print(results)
+# print(results.content)
